77cc8270ff80269dc2f89829bbe6184596dca9a8
kingofahn <kingofahn@gmail.com>
2019-06-16 22:20:56 +0900

    Initial Commit

---
+++ Web/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Web
+//
+//  Created by kingofahn on 16/06/2019.
+//  Copyright © 2019 kingofahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
